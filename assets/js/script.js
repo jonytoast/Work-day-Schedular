a7fd2c4a18b5962ea8692e306ec97fe8c31f111a
Jonathan Ortiz <jonathanortiz@jonathans-air.mynetworksettings.com>
2022-12-11 22:15:31 -0500

    added box feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  var today = dayjs();
-    $('#currentDay').text(today.format('dddd, MMMM D'));
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -11,16 +9,57 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
+    
+    // Applies past, present, future class based on each time
+
+    var currentHour = dayjs().hour;
+  
+    if (currentHour === 9) {
+      $('#hour-9').addClass('present');
+      $('#hour-10, #hour-11, #hour-12, #hour-1, #hour-2, #hour-3, #hour-4, #hour-5').addClass('future');
+    } else if (currentHour === 10){
+      $('#hour-9').addClass('past');
+      $('#hour-10').addClass('present');
+      $('#hour-11, #hour-12, #hour-1, #hour-2, #hour-3, #hour-4, #hour-5').addClass('future');
+    } else if (currentHour === 11){
+      $('#hour-9, #hour-10').addClass('past');
+      $('#hour-11').addClass('present');
+      $('#hour-12, #hour-1, #hour-2, #hour-3, #hour-4, #hour-5').addClass('future');
+    } else if (currentHour === 12){
+      $('#hour-9, #hour-10, #hour-11').addClass('past');
+      $('#hour-12').addClass('present');
+      $('#hour-1, #hour-2, #hour-3, #hour-4, #hour-5').addClass('future');
+    } else if (currentHour === 1){
+      $('#hour-9, #hour-10, #hour-11, #hour-12').addClass('past');
+      $('#hour-1').addClass('present');
+      $('#hour-2, #hour-3, #hour-4, #hour-5').addClass('future');
+    } else if (currentHour === 2){
+      $('#hour-9, #hour-10, #hour-11, #hour-12, #hour-1').addClass('past');
+      $('#hour-2').addClass('present');
+      $('#hour-3, #hour-4, #hour-5').addClass('future');
+    } else if (currentHour === 3){
+      $('#hour-9, #hour-10, #hour-11, #hour-12, #hour-1, #hour-2').addClass('past');
+      $('#hour-3').addClass('present');
+      $('#hour-4, #hour-5').addClass('future');
+    } else if (currentHour === 4){
+      $('#hour-9, #hour-10, #hour-11, #hour-12, #hour-1, #hour-2, #hour-3').addClass('past');
+      $('#hour-4').addClass('present');
+      $('#hour-5').addClass('future');
+    } else if (currentHour === 5){
+      $('#hour-9, #hour-10, #hour-11, #hour-12, #hour-1, #hour-2, #hour-3, #hour-4').addClass('past');
+      $('#hour-5').addClass('present');
+    } else {
+      $('#hour-9, #hour-10, #hour-11, #hour-12, #hour-1, #hour-2, #hour-3, #hour-4, #hour-5').addClass('future');
+    }
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
-    // TODO: Add code to display the current date in the header of the page.
+
+    // Display's the current date in the header of the page.
+    
+    var today = dayjs();
+    $('#currentDay').text(today.format('dddd, MMMM D'));
   });
   
